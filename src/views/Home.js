8ed4c2fc5af73853d8f05a583b257a5a210b404c
monibix <monibixv2@gmail.com>
2022-02-05 16:09:48 +0100

    fixed create

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import Button from '../components/Button';
 
 import Card from '../components/Card';
 import Form from '../components/Form';
 
+let initialFormState = { first_name:"", last_name:"", email:"" }
+
 const Home = () => {
 
     const [data, setData] = useState()
-    console.log('data state', data);
     const [isLoading, setIsLoading] = useState(true)
+    const [form, setForm] = React.useState(initialFormState)
+    const [message, setMessage] = useState("")
 
     useEffect(()=>{
         fetch('https://reqres.in/api/users?page=2')
@@ -21,49 +23,43 @@ const Home = () => {
     },[])
     
     const handleDelete = (id) => {
-        console.log('handle delete');
         setIsLoading(true)
         fetch(`https://reqres.in/api/users/${id}`)
             .then(res=>res.json())
             .then(response => {
                 let newList = [...data?.filter(item=>item.id !== response.data.id)]
-                console.log("NEW LIST", newList);
                 setData(newList)
                 setIsLoading(false)
             })
     }
 
-    let initialFormState = { first_name:"", last_name:"", email:"" }
-    const [form, setForm] = React.useState(initialFormState)
-
-
     const handleChange = (e) => {
-        console.log('e', e.target.value);
         const {name, value} = e.target;
         setForm({...form, [name]: value})
-        console.log("form", form);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setIsLoading(true)
         const newUser = {...form}
-        console.log("newUser", newUser);
 
-        fetch(`https://reqres.in/api/users`, {method: 'POST', body: JSON.stringify(newUser)})
+        fetch(`https://reqres.in/api/users`, 
+            {method: 'POST', 
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newUser)})
             .then(res=>res.json())
             .then(response => {
-                console.log('response', response.ok);
-                let newData = data.concat(newUser)
-                setData(newData) 
-                setIsLoading(false)
+                setMessage('User added successfully')
             })
+        
+        setForm(initialFormState)
+
     }
 
     return (
         <>  
             <h1>Users</h1>
                 <Form form={form} handleChange={handleChange} handleSubmit={handleSubmit} />
+                { message && <p className='user-msg'>{message}</p>}
             { isLoading && <p>Cargando...</p>}
             <div className="card-container">
             {
